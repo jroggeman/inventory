fa196c7d42472ba7533facbc5f60196a3f986065
Joel Roggeman <joelroggeman@gmail.com>
2016-06-19 23:51:04 -0400

    Setup RSpec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,37 @@
+RSpec.configure do |config|
+  # Use default assertions
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  # Use default mocks
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  # Sane defaults
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+  config.example_status_persistence_file_path = "spec/examples.txt"
+  config.disable_monkey_patching!
+
+  # Use more descriptive output for single tests
+  if config.files_to_run.one?
+    config.default_formatter = 'doc'
+  end
+
+  # Profile five slowest
+  config.profile_examples = 5 
+
+  # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = :random
+
+  # Seed global randomization in this process using the `--seed` CLI option.
+  # Setting this allows you to use `--seed` to deterministically reproduce
+  # test failures related to randomization by passing the same `--seed` value
+  # as the one that triggered the failure.
+  Kernel.srand config.seed
+end
