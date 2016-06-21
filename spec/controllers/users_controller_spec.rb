519bdcdca4f2f354ae8b8c47e8c8a8f06f661ce0
Joel Roggeman <joelroggeman@gmail.com>
2016-06-21 00:11:38 -0400

    Added user new, create, and show

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-  describe "GET /users/new" do
+  describe "GET new" do
     it "should assign @user" do
       get :new
       expect(assigns(:user)).to be_a_new(User)
@@ -10,6 +10,49 @@ RSpec.describe UsersController, type: :controller do
     it "should render the new template" do
       get :new
       expect(response).to render_template("new")
+    end
+  end
+
+  describe "GET show" do
+    it "should assign @user" do
+      user = create :user
+      get :show, id: user
+      expect(assigns(:user)).to eq(user)
+    end
+
+    it "should render the show template" do
+      user = create :user
+      get :show, id: user
+      expect(response).to render_template("show")
+    end
+  end
+
+  describe "POST create" do
+    context "with valid attributes" do
+      it "should create a new user" do
+        # user = build :user
+        expect {
+          post :create, user: attributes_for(:user)
+        }.to change(User, :count).by 1
+      end
+
+      it "should redirect to the user" do
+        post :create, user: attributes_for(:user)
+        expect(response).to redirect_to User.last
+      end
+    end
+
+    context "with invalid attributes" do
+      it "should not create a new user" do
+        expect {
+          post :create, user: attributes_for(:invalid_user)
+        }.to_not change(User, :count)
+      end
+
+      it "should render the new template" do
+        post :create, user: attributes_for(:invalid_user)
+        expect(response).to render_template("new")
+      end
     end
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,4 +5,8 @@ FactoryGirl.define do
     password_confirmation "12345678"
     name "Bob Schmidt"
   end
+
+  factory :invalid_user, parent: :user do
+    name nil
+  end
 end
