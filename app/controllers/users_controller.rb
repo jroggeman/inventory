3f9e146ec0b31e77b2cadac81e4c2eac9faf20f8
Joel Roggeman <joelroggeman@gmail.com>
2016-06-21 12:58:32 -0400

    Add update method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit]
+  before_action :set_user, only: [:show, :edit, :update]
 
   def new
     @user = User.new
@@ -19,6 +19,14 @@ class UsersController < ApplicationController
   end
 
   def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render "edit"
+    end
   end
 
   private
