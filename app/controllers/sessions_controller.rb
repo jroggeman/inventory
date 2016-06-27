d8f5700625c6f116ed04960929348954333f294a
Joel Roggeman <joelroggeman@gmail.com>
2016-06-27 19:37:27 -0400

    Add all backend login functionality

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: user_params[:email])
+
+    if user && user.authenticate(user_params[:password])
+      session[:id] = user.id
+      redirect_to User.find_by(email: user_params[:email])
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:id] = nil
+    render 'new'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+end
