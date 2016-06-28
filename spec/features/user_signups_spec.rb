7f2124b552a92b07d15c60fa36cddd199ecd983a
Joel Roggeman <joelroggeman@gmail.com>
2016-06-28 14:08:47 -0400

    Good and bad path integration tests

--- spec/features/user_signups_spec.rb
+++ spec/features/user_signups_spec.rb
@@ -15,5 +15,19 @@ RSpec.describe "UserSignups", type: :request do
       expect(page).to have_content('bob.schmidt@server.com')
       expect(page).to have_content('Bob Schmidt')
     end
+
+    it "doesn't create an invalid user" do
+      visit '/users/new'
+
+      fill_in 'Email', with: 'bob.schmidt@server.com'
+      fill_in 'Password', with: '1234'
+      fill_in 'Password confirmation', with: '123'
+      fill_in 'Name', with: 'Bob Schmidt'
+
+      click_on 'Register'
+
+      expect(page).to have_field('Email')
+      expect(page).to_not have_content('bob.schmidt@server.com')
+    end
   end
 end
