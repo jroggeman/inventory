0412d6ad36c3b66dd759fa32c83ed0104cb114f8
Joel Roggeman <joelroggeman@gmail.com>
2016-06-28 13:58:28 -0400

    Start work on request spec

---
+++ spec/requests/user_signups_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "UserSignups", type: :request do
+  describe "a valid registration process" do
+    it "creates a new user" do
+      visit '/users/new'
+
+      fill_in 'Email', with: 'bob.schmidt@server.com'
+      fill_in 'Password', with: '1234'
+      fill_in 'Password Confirmation', with: '1234'
+      fill_in 'Name', with: 'Bob Schmidt'
+
+      click_on 'Register'
+
+      expect(page).to have_content('bob.schmidt@server.com')
+      expect(page).to have_content('Bob Schmidt')
+    end
+  end
+end
