6a5307a4285d5055cfc8e95ecc797848fc7ce9dc
Joel Roggeman <joelroggeman@gmail.com>
2016-06-30 12:24:54 -0400

    Add GET show, GET new, POST create

--- app/controllers/boxes_controller.rb
+++ app/controllers/boxes_controller.rb
@@ -6,4 +6,23 @@ class BoxesController < ApplicationController
   def new
     @box = Box.new
   end
+
+  def create
+    box = Box.new(box_params)
+    if box.save
+      redirect_to box
+    else
+      render "new"
+    end
+  end
+
+  def show
+    @box = Box.find(params[:id])
+  end
+
+  private
+
+  def box_params
+    params.require(:box).permit(:name, :room, :content)
+  end
 end
--- spec/controllers/boxes_controller_spec.rb
+++ spec/controllers/boxes_controller_spec.rb
@@ -25,6 +25,15 @@ RSpec.describe BoxesController, type: :controller do
     end
   end
 
+  describe "GET show" do
+    it "should assign the expected box" do
+      box = create :box, user: @user
+
+      get :show, id: box
+      expect(assigns(:box)).to eq(Box.last)
+    end
+  end
+
   describe "GET new" do
     it "should render the new template" do
       get :new
@@ -34,6 +43,34 @@ RSpec.describe BoxesController, type: :controller do
     it "should assign @box" do
       get :new
       expect(assigns(:box)).to_not be nil
+    end
+  end
+
+  describe "POST create" do
+    context "with valid parameters" do
+      it "should create a new box" do
+        expect {
+          post :create, box: attributes_for(:box, user: @user)
+        }.to change(Box, :count).by(1)
+      end
+
+      it "should redirect to the box" do
+        post :create, box: attributes_for(:box, user: @user)
+        expect(response).to redirect_to Box.last
+      end
+    end
+
+    context "with invalid parameters" do
+      it "should not create a new box" do
+        expect {
+          post :create, box: attributes_for(:box, user: @user, name: nil)
+        }.to_not change(Box, :count)
+      end
+
+      it "should render the new template" do
+        post :create, box: attributes_for(:box, user: @user, name: nil)
+        expect(response).to render_template("new")
+      end
     end
   end
 end
